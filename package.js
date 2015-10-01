ed49e24b44674f1b6775ed52581f46e5bb1cb2da
tdamsma <tdamsma@gmail.com>
2015-10-01 15:12:08 +0200

    reduce unnecessary dependencies, and upgrade versions of others

--- package.js
+++ package.js
@@ -1,19 +1,17 @@
 Package.describe({
   'summary': 'Meteor account login via LDAP using activedirectory.js',
-  'version': '0.1.3',
+  'version': '0.1.4',
   'git' : 'https://github.com/tdamsma/meteor-accounts-ldap',
   'name' : 'tdamsma:meteor-accounts-ldap'
 });
 
-Npm.depends({'activedirectory' : '0.6.3' , 'connect' : '2.19.3'});
+Npm.depends({'activedirectory' : '0.7.0'});
 
 Package.on_use(function (api) {
-  api.use(['coffeescript@1.0.6','routepolicy@1.0.5', 'webapp@1.2.0'], 'server');
-  api.use(['accounts-base@1.2.0', 'underscore@1.0.3'], ['client', 'server']);
+  api.use(['coffeescript'], 'server');
+  api.use(['accounts-base'], ['client', 'server']);
   api.imply('accounts-base', ['client', 'server']);
-  //api.use('srp', ['client_functions', 'server']);
-  api.use(['ui@1.0.6', 'templating@1.1.1', 'jquery@1.11.3_2', 'spacebars@1.0.6'], 'client');
-  //api.export('LDAP','server');
+  api.use(['templating','jquery'], 'client');
   api.add_files([
     'ldap_client.html',
     'ldap_client.js'], 'client');
