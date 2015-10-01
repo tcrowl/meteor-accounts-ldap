d338214b65e11ae79cf5d4a74a43ee5f767925f2
tdamsma <tdamsma@gmail.com>
2015-10-01 15:08:22 +0200

    code formatting, remove redundant code

--- ldap_client.js
+++ ldap_client.js
@@ -1,21 +1,13 @@
-
-/*UI.registerHelper(
-  "ldapLogin",
-  function (option) {
-    return new Spacebars.SafeString(Template.ldapLogin());
-  }
-);*/
-
 var firstAttempt = true;
 
 Template.ldapLogin.events({
   'click button[name="login"]': function(e, tpl) {
-    initLogin(e,tpl);
+    initLogin(e, tpl);
   },
-  'keyup input' : function (e, tpl){
-    if (e.keyCode == 13){ //If Enter Key Pressed
-        initLogin(e,tpl)
-      }
+  'keyup input': function(e, tpl) {
+    if (e.keyCode == 13) { //If Enter Key Pressed
+      initLogin(e, tpl)
+    }
   },
   'click button[name="logout"]': function(e) {
     firstAttempt = true;
@@ -23,34 +15,35 @@ Template.ldapLogin.events({
   }
 });
 
-Meteor.loginWithLdap = function (username, password, callback) {
+Meteor.loginWithLdap = function(username, password, callback) {
   Accounts.callLoginMethod({
-    methodArguments: [{username: username, pass: password, ldap: true}],
-    validateResult: function (result) {
-    },
+    methodArguments: [{
+      username: username,
+      pass: password,
+      ldap: true
+    }],
+    validateResult: function(result) {},
     userCallback: callback
   });
 };
 
 Template.ldapLogin.helpers({
-  failedLogin : function(){
+  failedLogin: function() {
     return !firstAttempt; //return true if more than one attempt has been made. Show Error Message
   }
 })
 
 
 // Initiate Login Process:
-initLogin = function(e, tpl)
-{
+initLogin = function(e, tpl) {
   firstAttempt = false;
-    var username = $(tpl.find('input[name="ldap"]')).val();
-    var password = $(tpl.find('input[name="password"]')).val();
-    var result = Meteor.loginWithLdap(username, password, function() {
-      if (Meteor.userId())
-        return true;
-      else 
-        return false
-    });
-    return result;
+  var username = $(tpl.find('input[name="ldap"]')).val();
+  var password = $(tpl.find('input[name="password"]')).val();
+  var result = Meteor.loginWithLdap(username, password, function() {
+    if (Meteor.userId())
+      return true;
+    else
+      return false
+  });
+  return result;
 }
-
